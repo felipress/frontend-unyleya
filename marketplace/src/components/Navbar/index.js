6f71ba8832fcf3688551083864352a4afb032d3f
Felipe <phellyype@gmail.com>
2024-10-01 14:59:54 -0300

    Feat: creation of new projects

---
+++ marketplace/src/components/Navbar/index.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return (
+        <header className="bg-transparent z-50 w-full">
+            <nav className="flex items-center max-w-screen-xl mx-auto px-6 py-3">
+                <img src="" alt="Logo" className="w-36 cursor-pointer" />
+                <h1 className="text-center text-3xl font-semibold text-gray-700">Food App</h1>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar
