7c55f1db08ba9164bdea56da81bee431338566ff
Felipe <phellyype@gmail.com>
2024-10-16 10:23:03 -0300

    Fix: auth middleware at museum router

--- e-Museum/api/src/router/museum.router.js
+++ e-Museum/api/src/router/museum.router.js
@@ -1,11 +1,12 @@
 const router = require("express").Router()
 const MuseumController = require("../controller/museum.controller")
 const {validateIdParams} = require("../middleware/validation.middleware")
+const {auth, adminUser} = require("../middleware/auth.middleware")
 
 router.get("/findById/:id", validateIdParams, MuseumController.findMuseumById)
 router.get("/findAll", MuseumController.findAllMuseums)
-router.post("/create", MuseumController.createMuseum)
-router.put("/update/:id", MuseumController.updateMuseum)
-router.delete("/remove/:id", MuseumController.removeMuseum)
+router.post("/create", auth, adminUser, MuseumController.createMuseum)
+router.put("/update/:id", auth, adminUser, MuseumController.updateMuseum)
+router.delete("/remove/:id", auth, adminUser, MuseumController.removeMuseum)
 
 module.exports = router
